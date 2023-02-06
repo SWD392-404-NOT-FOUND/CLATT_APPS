a52a74f1495a75dd59ad3c8fbc54245ca32f3d38
THIEN <sherrin1st@gmail.com>
2023-02-06 10:35:12 +0700

    push code to github

---
+++ src/main/java/container/code/ClattAppsApplication.java
@@ -0,0 +1,13 @@
+package container.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClattAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClattAppsApplication.class, args);
+	}
+
+}
